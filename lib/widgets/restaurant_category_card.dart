0de9627eb7c037c735d729b23d48af9c5bc46651
Cristopher Yates <cmoyates@gmail.com>
2022-03-05 23:44:57 -0330

    Restyled the category list

--- lib/widgets/restaurant_category_card.dart
+++ lib/widgets/restaurant_category_card.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../models/resturant_categories.dart';
+import '../models/restaurant_categories.dart';
 
 class RestaurantCategoryCard extends StatelessWidget {
   const RestaurantCategoryCard({Key? key, required this.category}) : super(key: key);
@@ -8,33 +8,52 @@ class RestaurantCategoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: AlignmentDirectional.bottomCenter,
-      children: [
-        AspectRatio(
-          aspectRatio: 4/3,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(8),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: Stack(
+        alignment: AlignmentDirectional.bottomStart,
+        children: [
+          AspectRatio(
+            aspectRatio: 4/3,
             child: Image.asset(
               category.image,
               fit: BoxFit.cover,
+            )
+          ),
+          Positioned.fill(
+            child: Material(
+              color: Colors.transparent,
+              child: Ink(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.transparent, Colors.black],
+                    begin: Alignment.center,
+                    end: Alignment.bottomCenter
+                  )
+                ),
+                child: InkWell(
+                  onTap: ()=>{},
+                ),
+              )
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              bottom: 8,
+              left: 8
+            ),
+            child: Text(
+              category.label,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: Colors.white
+              ),
             ),
           )
-        ),
-        Padding(
-          padding: const EdgeInsets.only(
-            bottom: 8,
-          ),
-          child: Text(
-            category.label,
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w600
-            ),
-          ),
-        )
-      ],
-      
+        ],
+        
+      ),
     );
   }
 }
